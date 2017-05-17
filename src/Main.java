f4e24c939c6c36c42a5cf065a13cbd98e15fcb31
zhaoyangli <lizhaoyangmath@gmail.com>
2017-05-17 01:40:37 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello from Mikiel");
         System.out.println("Hello from Polina");
         System.out.println("Hello from Edwin");
+        System.out.println("Hello from Zhaoyang");
     }
 }

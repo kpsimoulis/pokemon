33a8493403e421b0d2fce6191440f1eb1c25551f
edwinyachoui <edwin.yachoui@gmail.com>
2017-05-24 21:27:34 -0400

    POK-16 initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import board.Board;
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,5 +13,11 @@ public class Main {
         System.out.println("Hello from Edwin");
         System.out.println("Hello from Zhaoyang");
         System.out.println("Hello from Xiaofang");
+
+//        JFrame frame = new JFrame("Board");
+//        frame.setContentPane(new Board().mainPanel);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
     }
 }
---
+++ src/board/Board.java
@@ -0,0 +1,68 @@
+package board;
+
+import card.Card;
+
+import javax.swing.*;
+
+/**
+ * Created by edwinyachoui on 2017-05-23.
+ */
+public class Board {
+    private JPanel mainPanel;
+    private JPanel playerPanel;
+    private JPanel playerSidePanel;
+    private JPanel playerHandCards;
+    private JPanel playerMainPanel;
+    private JPanel middlePanel;
+    private JPanel playerBenchCards;
+    private JPanel playerBenchPanel1;
+    private JButton playerBenchBtn1;
+    private JPanel playerBenchPanel2;
+    private JButton playerBenchBtn2;
+    private JPanel playerBenchPanel3;
+    private JButton playerBenchBtn3;
+    private JPanel playerBenchPanel4;
+    private JButton playerBenchBtn4;
+    private JPanel playerBenchPanel5;
+    private JButton playerBenchBtn5;
+    private JPanel playerHandPanel1;
+    private JButton playerHandBtn1;
+    private JPanel playerHandPanel2;
+    private JPanel playerHandPanel3;
+    private JPanel playerHandPanel4;
+    private JPanel playerHandPanel5;
+    private JPanel playerHandPanel6;
+    private JPanel playerHandPanel7;
+    private JButton playerHandBtn2;
+    private JButton playerHandBtn3;
+    private JButton playerHandBtn4;
+    private JButton playerHandBtn5;
+    private JButton playerHandBtn6;
+    private JButton playerHandBtn7;
+    private JPanel opponentPanel;
+    private JPanel opponentSidePanel;
+    private JPanel opponentCards;
+    private JPanel playerDeckPanel;
+    private JButton playerDeckBtn;
+    private JPanel playerDiscardPanel;
+    private JButton playerDiscardBtn;
+    private JPanel PokeBallPanel;
+    private JLabel PokeBallIcon;
+    private JPanel MiddleLeftPanel;
+    private JPanel MiddleRightPanel;
+    private JPanel opponentCoinPanel;
+    private JButton opponentCoinBtn;
+    private JPanel playerActivePanel;
+    private JButton playerActiveBtn;
+    private JPanel playerCoinPanel;
+    private JButton playerCoinBtn;
+    private JPanel opponentActivePanel;
+    private JButton opponentActiveBtn;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+
+
+}

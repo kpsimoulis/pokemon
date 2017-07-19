af9dca13e5f96abb64dd564f5b76f6c2509ceb1e
Konstantinos Psimoulis <kpsimoulis@summit-tech.ca>
2017-07-19 04:27:34 -0400

    POK-57 Initial commit

---
+++ src/main/java/parser/Main.java
@@ -0,0 +1,18 @@
+package parser;
+
+import controllers.game.GameController;
+
+import javax.swing.*;
+
+
+public class Main implements Runnable{
+
+    public void run() {
+        GameController mainControl = new GameController();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Main());
+    }
+
+}
